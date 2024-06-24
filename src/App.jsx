89e5c578899c7577a3498ac2e9d9a6c6f68001ef
Malo <42148894+Jivadio@users.noreply.github.com>
2024-06-24 14:05:16 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-slate-800">
+            <div className="max-w-7xl mx-auto py-20 px-4">
+                <h1 className="text-gray-50 text-4xl">Données des pays d'Europe</h1>
+                <p className="text-gray-100 text-xl mb-8">Cliquez sur une carte pour révéler les informations du
+                    pays.</p>
+            </div>
+        </div>
+    )
+}
+
+export default App
